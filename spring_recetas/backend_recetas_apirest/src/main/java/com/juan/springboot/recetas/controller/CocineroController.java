235dfdced62324645502f113a73695c7b71d95b4
jprogramcourses <jprogramcourses@gmail.com>
2022-10-01 22:20:26 +0200

    Se añaden anotaciones swagger

---
+++ spring_recetas/backend_recetas_apirest/src/main/java/com/juan/springboot/recetas/controller/CocineroController.java
@@ -0,0 +1,33 @@
+package com.juan.springboot.recetas.controller;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+
+import com.juan.springboot.recetas.entity.Cocinero;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+public interface CocineroController {
+	
+	@ApiOperation(value = "${controller.cocinero.getcocineros}")
+	public List<Cocinero> getCocineros();
+	
+	@ApiOperation(value = "List all cocineros by page")
+	public Page<Cocinero> getCocineros(Integer page);
+	
+	@ApiOperation(value = "Retrieve cocinero select by id")
+	public ResponseEntity<?> show(
+			@ApiParam(value = "Cocinero id")
+			Integer idCocinero);
+	
+	public ResponseEntity<?> create(Cocinero cocinero, BindingResult result);
+	
+	public ResponseEntity<?> update(Cocinero cocinero, BindingResult result, Integer idCocinero);
+
+	public ResponseEntity<?> delete(Integer idCocinero);
+	
+}
